076a88d9bdea77132c0f218554f4c71071dcc0a9
Jonatan Bernal <addicting97@gmail.com>
2019-03-13 23:42:33 +0100

    Initial commit

---
+++ app/src/main/java/com/bernal/jonatan/whip/MainActivity.java
@@ -0,0 +1,39 @@
+package com.bernal.jonatan.whip;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button login_button, signin_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        login_button = (Button) findViewById(R.id.login);
+        signin_button = (Button) findViewById(R.id.signin);
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        signin_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //startActivity(new Intent(MainActivity.this, SignInScreen.class));
+                finish();
+            }
+        });
+
+
+    }
+}
