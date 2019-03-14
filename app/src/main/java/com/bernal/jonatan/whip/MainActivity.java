7d34c30c3774cf34886523c266db772679949b6c
Jonatan Bernal <addicting97@gmail.com>
2019-03-14 01:55:05 +0100

    Commit

--- app/src/main/java/com/bernal/jonatan/whip/MainActivity.java
+++ app/src/main/java/com/bernal/jonatan/whip/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
         login_button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v) { //comentario
 
             }
         });
