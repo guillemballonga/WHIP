618843155fe7cf5611ba78b58d5ea67a8d49a552
guillemballonga <47830285+guillemballonga@users.noreply.github.com>
2019-03-28 16:06:12 +0100

    Pantalla Perfil amb back (Error a la crida??)

--- app/src/main/java/com/bernal/jonatan/whip/EditarPerfil.java
+++ app/src/main/java/com/bernal/jonatan/whip/EditarPerfil.java
@@ -13,6 +13,21 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class EditarPerfil extends AppCompatActivity {
 
@@ -20,6 +35,10 @@ public class EditarPerfil extends AppCompatActivity {
     Button goToMostrarPerfilGuardant, goToMostrarPerfilCancelar;
     ImageView fotoperfil;
     EditText nom,cognom,user,cp;
+
+    //variables para comucicación back
+    private String URL;
+    private RequestQueue requestqueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,10 +56,15 @@ public class EditarPerfil extends AppCompatActivity {
         fotoperfil = (ImageView) findViewById(R.id.imagen_perfil);
 
 
+
         //Gestión de las Toolbars
         Toolbar tool = (Toolbar) findViewById(R.id.toolbar_editarPerfil);
         setSupportActionBar(tool);
         getSupportActionBar().setTitle("PERFIL");
+
+        //Coneixón con la API
+        URL = "https://whip-api.herokuapp.com/users/profile";
+        requestqueue = Volley.newRequestQueue(this);
 
 
 
@@ -48,14 +72,64 @@ public class EditarPerfil extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //Comunicacion con Back
+                //jason para comunicación con back
+                JSONObject perfil_editat = new JSONObject();
+
+
+                //JASON
+                try {
+                    perfil_editat.put("post_code", cp.getText().toString());
+                    perfil_editat.put("name", nom.getText().toString() );
+                    perfil_editat.put("about","");
+                    perfil_editat.put("fam_name", cognom.getText().toString());
+                    perfil_editat.put("username", user.getText().toString());
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
 
                 if(nom.getText().toString().equals("") || cp.getText().toString().equals("") || cognom.getText().toString().equals("") || user.getText().toString().equals("")) {
-                    Toast.makeText(getApplicationContext(),"Todos los campos son obligatorios",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Todos los campos son obligatorios", Toast.LENGTH_SHORT).show();
                 }
-                else {
-                    startActivity(new Intent(EditarPerfil.this, MostrarPerfil.class));
-                    finish();
+                else{
+                    //Guardar los datos del formulario en BACK. NOTA: No olvidar guardar la fecha de creación del Post
+                    JsonObjectRequest objectJsonrequest = new JsonObjectRequest(
+                            JsonRequest.Method.POST,
+                            URL,
+                            perfil_editat,
+                            new Response.Listener<JSONObject>() {
+                                @Override
+                                public void onResponse(JSONObject response) {
+                                    Toast.makeText(getApplicationContext(),"Perfil actualizado con éxito",Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(EditarPerfil.this, MostrarPerfil.class));
+                                    finish();
+                                }
+
+
+                            },
+                            new Response.ErrorListener() {
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+                                    Toast.makeText(getApplicationContext(),"ERROOOOOOOR",Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                    ) {
+                        @Override
+                        public Map<String, String> getHeaders() throws AuthFailureError {
+                            Map<String, String> params = new HashMap<String, String>();
+                            params.put("Content-Type", "application/json");
+                            params.put("Authorization", "2C4T55N-4SY40G3-JBG7QMB-4PYNJ9P"); //valor de V ha de ser el de la var global
+                            return params;
+                        }
+                    };
+                    requestqueue.add(objectJsonrequest);
+//JASON
+
+
+
                 }
+
             }
             
         });
