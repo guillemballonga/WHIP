0581f96880196e682a606741f04b7cc0c59dab73
Jonatan Bernal <addicting97@gmail.com>
2019-04-03 02:31:00 +0200

    Navigation casi en proceso

--- app/src/main/java/com/bernal/jonatan/whip/Menu_principal.java
+++ app/src/main/java/com/bernal/jonatan/whip/Menu_principal.java
@@ -1,5 +1,9 @@
 package com.bernal.jonatan.whip;
 
+import android.app.NativeActivity;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -7,7 +11,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
-public class Menu_principal extends AppCompatActivity {
+public class Menu_principal extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
@@ -27,6 +31,8 @@ public class Menu_principal extends AppCompatActivity {
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        NavigationView nv = (NavigationView) findViewById(R.id.navigation_view);
+        nv.setNavigationItemSelectedListener(this);
 
     }
 
@@ -37,5 +43,30 @@ public class Menu_principal extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+       switch (menuItem.getItemId()){
+           case R.id.nav_encontrado_perdido:
+               startActivity(new Intent(Menu_principal.this, ListadoPerdida.class));
+               break;
+           case R.id.nav_adopcion:
+
+               break;
+           case R.id.nav_VerPerfil:
+
+               startActivity(new Intent(Menu_principal.this, MostrarPerfil.class));
+               break;
+           case R.id.nav_eventos:
+
+               break;
+           case R.id.nav_logout:
+
+               break;
+       }
+
+
+        return false;
     }
 }
