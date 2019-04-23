85c609869782b5ed42a81290e5e5c389293c6f67
Jonatan Bernal <addicting97@gmail.com>
2019-04-23 19:18:38 +0200

    Revert "Merge branch 'imatges_sincronitzacio'"
    This reverts commit 74efc2410ac3c4f9e9cab151a46d7461459dcd8d, reversing
    changes made to aed85f5c6b0757c0babf794b8c114bc3ac60f2be.

--- app/src/main/java/com/bernal/jonatan/whip/UploadImageFirebase.java
+++ app/src/main/java/com/bernal/jonatan/whip/UploadImageFirebase.java
@@ -3,7 +3,6 @@ package com.bernal.jonatan.whip;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
@@ -17,7 +16,6 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.UUID;
 
@@ -26,7 +24,6 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.storage.FileDownloadTask;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
@@ -41,11 +38,6 @@ public class UploadImageFirebase extends AppCompatActivity {
 
     private Button btnChoose, btnUpload;
     private ImageView imageView;
-    private  ImageView imageViewPosar;
-
-   // int idImageView;
-
-    String idActivity = "";
 
     private Uri filePath;
 
@@ -54,13 +46,8 @@ public class UploadImageFirebase extends AppCompatActivity {
     public static String getIdentificadorImatge() {
         return identificadorImatge;
     }
-    public static void netejaIdentificadorImatge() {
-         identificadorImatge = "";
-    }
 
     private static String identificadorImatge  = "";
-
-
 
     //Firebase
     private FirebaseStorage storage;
@@ -80,13 +67,6 @@ public class UploadImageFirebase extends AppCompatActivity {
         //Initialize firebase
         storage = FirebaseStorage.getInstance();
         storageReference = storage.getReference();
-
-
-        //agafo l id del imageview
-
-        idActivity = getIntent().getStringExtra("idActivity");
-
-
 
         btnChoose.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -151,24 +131,10 @@ public class UploadImageFirebase extends AppCompatActivity {
                         @Override
                         public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
                             //retornar a l altre pantalla l identificador perque back guardi
-
+                            finish();
                             String xxx = identificadorImatge;
 
-
                             // startActivity(new Intent(UploadImageFirebase.this, ShowImage.class));
-
-                            //if (idImageView != 0 && imageViewPosar != null) {
-                            if (idActivity.equals("lost"))
-                               NewPostLost.retrieveImage(xxx);
-
-                            else if (idActivity.equals("adoption")){
-                                NewPostAdoption.retrieveImage(xxx);
-
-                            }
-
-                            finish();
-                          // startActivity(new Intent(UploadImageFirebase.this, ShowImage.class));
-
                         }
                     })
                     .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
@@ -197,30 +163,6 @@ public class UploadImageFirebase extends AppCompatActivity {
                         }
                     });
         }
-    }
-
-    public void retrieveImage(String idImageFirebase) {
-
-        FirebaseStorage storage = FirebaseStorage.getInstance();
-        //TODO: necessito recuperar l objecte desde el json. a child posarhi l indetificador guardat
-        StorageReference storageReference = storage.getReferenceFromUrl("gs://whip-1553341713756.appspot.com/").child(idImageFirebase);
-
-       // imageView = (ImageView) findViewById(R.id.imageFirebase);
-        try {
-            final File localFile = File.createTempFile("images", "jpg");
-            storageReference.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
-                @Override
-                public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
-                    Bitmap bitmap = BitmapFactory.decodeFile(localFile.getAbsolutePath());
-                    imageViewPosar.setImageBitmap(bitmap);
-
-                }
-            }).addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception exception) {
-                }
-            });
-        } catch (IOException e ) {}
     }
 
 
