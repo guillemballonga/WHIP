6ca00e499b66a8ca3ab62ba729bd7a8433628995
Laura <lauramunne@e-binari.com>
2019-05-21 09:23:27 +0200

    dividint en funcionetes la class calendar

--- app/src/main/java/com/bernal/jonatan/whip/Calendar.java
+++ app/src/main/java/com/bernal/jonatan/whip/Calendar.java
@@ -13,8 +13,7 @@ import com.google.api.client.auth.oauth2.Credential;
         import com.google.api.client.json.jackson2.JacksonFactory;
         import com.google.api.client.util.DateTime;
         import com.google.api.client.util.store.FileDataStoreFactory;
-        import com.google.api.services.calendar.Calendar;
-        import com.google.api.services.calendar.CalendarScopes;
+import com.google.api.services.calendar.CalendarScopes;
         import com.google.api.services.calendar.model.Event;
         import com.google.api.services.calendar.model.Events;
 
@@ -26,7 +25,7 @@ import com.google.api.client.auth.oauth2.Credential;
         import java.util.Collections;
         import java.util.List;
 
-public class CalendarQuickstart {
+public class Calendar {
 
     private static final String APPLICATION_NAME = "Google Calendar API Java Quickstart";
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
@@ -47,7 +46,7 @@ public class CalendarQuickstart {
      */
     private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
         // Load client secrets.
-        InputStream in = CalendarQuickstart.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        InputStream in = Calendar.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
         if (in == null) {
             throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
         }
@@ -64,13 +63,50 @@ public class CalendarQuickstart {
     }
 
 
-
-    public static void main(String... args) throws IOException, GeneralSecurityException {
+    public com.google.api.services.calendar.Calendar apiCalendar() throws GeneralSecurityException, IOException {
         // Build a new authorized API client service.
         final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-        Calendar service = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+        com.google.api.services.calendar.Calendar service = new com.google.api.services.calendar.Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
                 .setApplicationName(APPLICATION_NAME)
                 .build();
+        getCredentials(HTTP_TRANSPORT);
+        return service;
+
+    }
+    public void listEventsFromPrimaryCalendar(com.google.api.services.calendar.Calendar service) throws IOException {
+        // List the next 10 events from the primary calendar.
+        DateTime now = new DateTime(System.currentTimeMillis());
+        Events events = service.events().list("primary")
+                .setMaxResults(10)
+                .setTimeMin(now)
+                .setOrderBy("startTime")
+                .setSingleEvents(true)
+                .execute();
+        List<Event> items = events.getItems();
+        if (items.isEmpty()) {
+            System.out.println("No upcoming events found.");
+        } else {
+            System.out.println("Upcoming events");
+            for (Event event : items) {
+                DateTime start = event.getStart().getDateTime();
+                if (start == null) {
+                    start = event.getStart().getDate();
+                }
+                System.out.printf("%s (%s)\n", event.getSummary(), start);
+            }
+        }
+    }
+
+
+
+    public static void main(String... args) throws IOException, GeneralSecurityException {
+
+        // Build a new authorized API client service.
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        com.google.api.services.calendar.Calendar service = new com.google.api.services.calendar.Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+
 
         // List the next 10 events from the primary calendar.
         DateTime now = new DateTime(System.currentTimeMillis());
