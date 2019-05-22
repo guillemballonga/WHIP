e1d3ed4114e58fb1e3137fc4d99adbe169afe32f
guillemballonga <47830285+guillemballonga@users.noreply.github.com>
2019-05-22 19:29:31 +0200

    canvis dia de presentacio

--- app/src/main/java/com/bernal/jonatan/whip/Views/InfoEvent.java
+++ app/src/main/java/com/bernal/jonatan/whip/Views/InfoEvent.java
@@ -28,7 +28,6 @@ public class InfoEvent extends AppCompatActivity implements EventPresenter.View{
     private Button acceptarQuedada, rebutjarQuedada;
     private UserLoggedIn ul = UserLoggedIn.getUsuariLogejat("", "", "");
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,16 +35,29 @@ public class InfoEvent extends AppCompatActivity implements EventPresenter.View{
 
         idEvent = getIntent().getStringExtra("idEvent");
 
+<<<<<<< HEAD
 
         Toast.makeText(getApplicationContext(), "token usu " + ul.getToken(), Toast.LENGTH_SHORT).show();
         String xxx = ul.getToken();
         System.out.println("token id: " + ul.getToken());
 
 
+=======
+>>>>>>> CarregarInfoEvent
         acceptarQuedada = findViewById(R.id.boto_acceptar_event);
         rebutjarQuedada = findViewById(R.id.boto_rebutjar_event);
+        URL = "https://whip-api.herokuapp.com/event/" + idEvent;
         URLUpdateAccept = "https://whip-api.herokuapp.com/event/" + idEvent + "/answer?action=accept";
         URLUpdateReject = "https://whip-api.herokuapp.com/event/" + idEvent + "/answer?action=reject";
+
+        titulo = findViewById(R.id.titulo_post_provinente);
+        fechaPost = findViewById(R.id.fecha_post_provinente);
+        fechaQuedada = findViewById(R.id.fecha_info_quedada);
+        hora = findViewById(R.id.hora_info_quedada);
+        lugar = findViewById(R.id.lugar_info_quedada);
+        idSolicitante = findViewById(R.id.id_creadorQuedada);
+
+
 
         acceptarQuedada.setOnClickListener(new View.OnClickListener() {
 
@@ -54,6 +66,8 @@ public class InfoEvent extends AppCompatActivity implements EventPresenter.View{
 
                 try {
                     eventPresenter.updateEvent(URLUpdateAccept);
+                    startActivity(new Intent(InfoEvent.this, EventList.class));
+                    finish();
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -84,9 +98,23 @@ public class InfoEvent extends AppCompatActivity implements EventPresenter.View{
             }
         });
 
+        eventPresenter.getEventInfo(URL);
+
     }
 
 
+    @Override
+    public void setEvent(String UserFromPostId, String UserId, String Place, String Date, String Time) {
+        //titulo = findViewById(R.id.titulo_post_provinente);
+        //fechaPost = findViewById(R.id.fecha_post_provinente);
+        fechaQuedada.setText(Date);
+        hora.setText(Time);
+        lugar.setText(Place);
+        idSolicitante.setText(UserFromPostId);
+
+
+
+    }
 
     @Override
     public void chargeEvents(ArrayList events) {
