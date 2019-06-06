96afe2989e61b9707e2a3290ddb71f6270947608
Jonatan Bernal <addicting97@gmail.com>
2019-06-06 19:20:47 +0200

    Cambios de color a rosa

--- app/src/main/java/com/bernal/jonatan/whip/Servers/ChatServer.java
+++ app/src/main/java/com/bernal/jonatan/whip/Servers/ChatServer.java
@@ -18,7 +18,6 @@ import com.bernal.jonatan.whip.Views.UserLoggedIn;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
